fe084491db3684de8af16a811d321f5151823258
Miguel S Peña <113079863+PenaSMiguel92@users.noreply.github.com>
2023-08-13 16:05:17 -0700

    first solution attempt

--- Main.java
+++ Main.java
@@ -7,9 +7,23 @@ public class Main {
         ListNode list1_node1 = new ListNode(2, list1_node2);
 
         ListNode list2_node3 = new ListNode(4);
-        ListNode list2_node2 = new ListNode(6, list1_node3);
-        ListNode list2_node1 = new ListNode(5, list1_node2);
+        ListNode list2_node2 = new ListNode(6, list2_node3);
+        ListNode list2_node1 = new ListNode(5, list2_node2);
+
+        ListNode list3_node7 = new ListNode(9);
+        ListNode list3_node6 = new ListNode(9, list3_node7);
+        ListNode list3_node5 = new ListNode(9, list3_node6);
+        ListNode list3_node4 = new ListNode(9, list3_node5);
+        ListNode list3_node3 = new ListNode(9, list3_node4);
+        ListNode list3_node2 = new ListNode(9, list3_node3);
+        ListNode list3_node1 = new ListNode(9, list3_node2);
+
+        ListNode list4_node4 = new ListNode(9);
+        ListNode list4_node3 = new ListNode(9, list4_node4);
+        ListNode list4_node2 = new ListNode(9, list4_node3);
+        ListNode list4_node1 = new ListNode(9, list4_node2);
 
         System.out.println(sol.addTwoNumbers(list1_node1, list2_node1));
+        System.out.println(sol.addTwoNumbers(list3_node1, list4_node1));
     }
 }
--- Solution.java
+++ Solution.java
@@ -1,5 +1,28 @@
 public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return new ListNode();
+        ListNode answer = new ListNode();
+        ListNode answerPointer = answer;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum > 9 ? 1 : 0;
+
+            answerPointer.val = sum % 10;
+            answerPointer.next = new ListNode();
+            answerPointer = answerPointer.next;
+        }
+        if (carry != 0) {
+            answerPointer.val = carry;
+        }
+        return answer;
     }
 }
